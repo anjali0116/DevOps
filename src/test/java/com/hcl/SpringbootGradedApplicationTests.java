1c4e95373c0f17ad2863467d611e6b7fb879c1c7
anjali0116 <anjalimadishetti11@gmail.com>
2021-06-05 22:18:58 +0530

    first commit

---
+++ src/test/java/com/hcl/SpringbootGradedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGradedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
